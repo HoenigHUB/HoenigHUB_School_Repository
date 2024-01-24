972ad0ab0ad85c3a93d301cf617b5db599513c09
Jarrett Hoenig <156244362+HoenigHUB@users.noreply.github.com>
2024-01-24 16:21:52 -0500

    added coin and enemy
    added objects no sound yet, jump does not work

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 0f;
+    public float JumpHeight = 0f;
 
     private float horizinput;
     private Rigidbody2D rb;
@@ -17,6 +18,14 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizinput = Input.GetAxisRaw("Horizontal");
+        if(Input.GetKeyDown(KeyCode.Space) )
+        {
+            rb.velocity = Vector2.up * JumpHeight;
+        }
+    } 
+
+    private void FixedUpdate()
+    {
         rb.velocity = new Vector2(horizinput * speed, rb.velocity.y);
     }
 }
